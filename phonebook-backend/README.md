167a61671157e9df64aa6d4e4e6f6cadb9b597b7
Nick Meinhold <nick@enspyr.co>
2025-11-06 18:06:37 +1100

    Completing 3.10 & 3.11

---
+++ phonebook-backend/README.md
@@ -0,0 +1,5 @@
+# Fullstackopen Part 3 Backend
+
+The backend is live at:
+
+[https://api-j7rx7hkrga-uc.a.run.app/](https://api-j7rx7hkrga-uc.a.run.app/)
--- phonebook-frontend/src/services/personsService.js
+++ phonebook-frontend/src/services/personsService.js
@@ -1,25 +1,24 @@
 import axios from "axios";
 
+const baseUrl = "https://api-j7rx7hkrga-uc.a.run.app/";
+
 const getAll = () => {
-  const request = axios.get("http://localhost:3001/api/all-persons");
+  const request = axios.get(`${baseUrl}/api/all-persons`);
   return request.then((response) => response.data);
 };
 
 const create = (newObject) => {
-  const request = axios.post("http://localhost:3001/api/persons", newObject);
+  const request = axios.post(`${baseUrl}/api/persons`, newObject);
   return request.then((response) => response.data);
 };
 
 const update = (id, newObject) => {
-  const request = axios.put(
-    `http://localhost:3001/api/persons/${id}`,
-    newObject
-  );
+  const request = axios.put(`${baseUrl}/api/persons/${id}`, newObject);
   return request.then((response) => response.data);
 };
 
 const deletePerson = (id) => {
-  const request = axios.delete(`http://localhost:3001/api/persons/${id}`);
+  const request = axios.delete(`${baseUrl}/api/persons/${id}`);
   return request;
 };
 
