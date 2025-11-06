de5c480a31bbcb3ecba69a09d432fdf518ab0820
Nick Meinhold <nick@enspyr.co>
2025-11-06 15:17:41 +1100

    Completed 3.7

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 let persons = [
   {
@@ -24,16 +25,8 @@ let persons = [
   },
 ];
 
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
-
 app.use(express.json());
-app.use(requestLogger);
+app.use(morgan("tiny"));
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
